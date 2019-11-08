40d7f820ace0baeae6eac9805efb95bd226b7563
kieulytt <kieulydt@gmail.com>
2019-11-08 14:41:33 +0700

    gsmarena 3

--- spiders/gsmarena.py
+++ spiders/gsmarena.py
@@ -3,19 +3,23 @@ import scrapy
 
 class Mobiles(scrapy.Spider):
     name = 'gsmarena'
-    start_urls = ['http://webcache.googleusercontent.com/search?q=cache:https://www.gsmarena.com/makers.php3']
+    start_urls = ['http://webcache.googleusercontent.com/search?q=cache:https://www.gsmarena.com/']
+    domain = 'http://webcache.googleusercontent.com/search?q=cache:'
+    gsm_domain = 'https://www.gsmarena.com/'
 
     def start_requests(self):
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parse_brand)
 
+
     def parse_brand(self, response):
         for item in response.css('div.brandmenu-v2 li'):
             if item is not None:
-                next_url = item.css('a::href').extract_first()
+                next_url = item.css('a::attr(href)').extract_first()
+                next_url = self.domain + self.gsm_domain + next_url
                 brand = item.css('a::text').extract_first()
-                next_page = response.urljoin(next_url)
-                request = scrapy.Request(url=next_page, callback=self.parse_item)
+    #             next_page = response.urljoin(next_url)
+                request = scrapy.Request(url=next_url, callback=self.parse_item)
                 request.meta['brand'] = brand
                 yield request
 
