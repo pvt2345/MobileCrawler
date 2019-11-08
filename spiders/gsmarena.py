10902fe00be3f4e565b1e9dce7c97f1e432cfab5
kieulydt <48474668+kieulydt@users.noreply.github.com>
2019-11-08 14:56:20 +0700

    Add files via upload

--- spiders/gsmarena.py
+++ spiders/gsmarena.py
@@ -3,7 +3,9 @@ import scrapy
 
 class Mobiles(scrapy.Spider):
     name = 'gsmarena'
-    start_urls = ['http://webcache.googleusercontent.com/search?q=cache:https://www.gsmarena.com/makers.php3']
+    start_urls = ['http://webcache.googleusercontent.com/search?q=cache:https://www.gsmarena.com/']
+    domain = 'http://webcache.googleusercontent.com/search?q=cache:'
+    gsm_domain = 'https://www.gsmarena.com/'
 
     def start_requests(self):
         for url in self.start_urls:
@@ -12,11 +14,21 @@ class Mobiles(scrapy.Spider):
     def parse_brand(self, response):
         for item in response.css('div.brandmenu-v2 li'):
             if item is not None:
-                next_url = item.css('a::href').extract_first()
+                next_url = item.css('a::attr(href)').extract_first()
+                next_url = self.domain + self.gsm_domain + next_url
                 brand = item.css('a::text').extract_first()
-                next_page = response.urljoin(next_url)
-                request = scrapy.Request(url=next_page, callback=self.parse_item)
+                #             next_page = response.urljoin(next_url)
+                request = scrapy.Request(url=next_url, callback=self.parse_list_item)
                 request.meta['brand'] = brand
+                yield request
+
+    def parse_list_item(self, response):
+        for item in response.css('div.makers li'):
+            if item is not None:
+                next_url = item.css('a::attr(href)').extract_first()
+                next_url = self.domain + self.gsm_domain + next_url
+                request = scrapy.Request(url=next_url, callback=self.parse_item)
+                request.meta['brand'] = response.meta['brand']
                 yield request
 
     def parse_item(self, response):
